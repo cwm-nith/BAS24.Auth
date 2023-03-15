ad1ace729ad18b9989121603f030ead26c5716a3
yaty1031 <yaty1031@gmail.com>
2023-03-15 19:53:40 +0700

    feat: Currency Repository

--- ProductApi/BAS24.Product.Core/IRepositories/ICurrencyRepository.cs
+++ ProductApi/BAS24.Product.Core/IRepositories/ICurrencyRepository.cs
@@ -1,6 +1,17 @@
+using BAS24.Libs.CQRS.Queries;
+using BAS24.Product.Core.Entities.Currency;
+
 namespace BAS24.Product.Core.IRepositories;
 
 public interface ICurrencyRepository
 {
-  
+  Task AddAsync(CurrencyEntity c);
+  Task UpdateAsync(CurrencyEntity c);
+  Task<PagedResult<CurrencyEntity>> GetAllByStoreOwnerAsync(Guid storeOwnerId, PagedQuery pagedQuery);
+  Task<PagedResult<CurrencyEntity>> GetAllActiveByStoreOwnerAsync(Guid storeOwnerId, PagedQuery pagedQuery);
+  Task<PagedResult<CurrencyEntity>> GetAllAsync(PagedQuery pagedQuery);
+  Task<CurrencyEntity?> GetByIdAsync(Guid id);
+  Task<CurrencyEntity?> GetBaseCurrencyAsync(Guid storeOwnerId);
+  Task<CurrencyEntity?> GetLocalCurrencyAsync(Guid storeOwnerId);
+  Task DeleteAsync(Guid id);
 }
