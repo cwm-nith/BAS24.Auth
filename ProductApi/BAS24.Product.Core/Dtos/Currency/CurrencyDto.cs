79dd8895bea998dcffe1373c790f330c362daf43
yaty1031 <yaty1031@gmail.com>
2023-03-15 08:06:03 +0700

    feat: currency table

---
+++ ProductApi/BAS24.Product.Core/Dtos/Currency/CurrencyDto.cs
@@ -0,0 +1,38 @@
+namespace BAS24.Product.Core.Dtos.Currency;
+
+public class CurrencyDto
+{
+  public Guid Id { get; set; }
+  public string Symbol { get; set; }
+
+  public string Description { get; set; }
+
+  public bool Active { get; set; }
+
+  public bool BaseCurrency { get; set; }
+
+  public bool LocalCurrency { get; set; }
+
+  public DateTime CreatedAt { get; set; }
+
+  public DateTime UpdatedAt { get; set; }
+
+  public CurrencyDto(Guid id,
+    string symbol,
+    string description,
+    bool active,
+    bool baseCurrency,
+    bool localCurrency,
+    DateTime createdAt,
+    DateTime updatedAt)
+  {
+    Id = id;
+    Symbol = symbol;
+    Description = description;
+    Active = active;
+    BaseCurrency = baseCurrency;
+    LocalCurrency = localCurrency;
+    CreatedAt = createdAt;
+    UpdatedAt = updatedAt;
+  }
+}
---
+++ ProductApi/BAS24.Product.Infrastructure/Postgres/Currency/Extensions.cs
@@ -0,0 +1,38 @@
+using BAS24.Product.Core.Dtos.Currency;
+using BAS24.Product.Core.Entities.Currency;
+
+namespace BAS24.Product.Infrastructure.Postgres.Currency;
+
+public static class Extensions
+{
+  public static CurrencyEntity AsEntity(this CurrencyTable c) => new(
+    id: c.Id,
+    symbol: c.Symbol,
+    description: c.Description,
+    active: c.Active,
+    baseCurrency: c.BaseCurrency,
+    localCurrency: c.LocalCurrency,
+    createdAt: c.CreatedAt,
+    updatedAt: c.UpdatedAt
+  );
+
+  public static CurrencyTable AsTable(this CurrencyEntity c) => new(
+    symbol: c.Symbol,
+    description: c.Description,
+    active: c.Active,
+    baseCurrency: c.BaseCurrency,
+    localCurrency: c.LocalCurrency,
+    updatedAt: c.UpdatedAt
+  ) { CreatedAt = c.CreatedAt, Id = c.Id };
+
+  public static CurrencyDto AsDto(this CurrencyEntity c) => new(
+    id: c.Id,
+    symbol: c.Symbol,
+    description: c.Description,
+    active: c.Active,
+    baseCurrency: c.BaseCurrency,
+    localCurrency: c.LocalCurrency,
+    createdAt: c.CreatedAt,
+    updatedAt: c.UpdatedAt
+  );
+}
