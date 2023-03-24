6d05ac214e4a2952bac234c28aca5bffb674610e
yaty1031 <yaty1031@gmail.com>
2023-03-24 07:52:24 +0700

    impliment kafka producer service

--- AuthApi/BAS24.Auth.Infrastructure/Services/Interfaces/IKafkaProducers.cs
+++ AuthApi/BAS24.Auth.Infrastructure/Services/Interfaces/IKafkaProducers.cs
@@ -2,5 +2,5 @@ namespace BAS24.Auth.Infrastructure.Services.Interfaces;
 
 public interface IKafkaProducers
 {
-  Task<bool> SendAsync<T>(T data);
+  Task<bool> SendAsync<T>(T data, string topic) where T : class;
 }
--- AuthApi/BAS24.Auth.Infrastructure/Services/KafkaProducers.cs
+++ AuthApi/BAS24.Auth.Infrastructure/Services/KafkaProducers.cs
@@ -1,11 +1,47 @@
+using System.Diagnostics;
+using System.Net;
 using BAS24.Auth.Infrastructure.Services.Interfaces;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace BAS24.Auth.Infrastructure.Services;
 
-public class KafkaProducers:IKafkaProducers
+public class KafkaProducers : IKafkaProducers
 {
-  public Task<bool> SendAsync<T>(T data)
+  private readonly IConfiguration _configuration;
+
+  public KafkaProducers(IConfiguration configuration)
   {
-    throw new NotImplementedException();
+    _configuration = configuration;
+  }
+
+  public async Task<bool> SendAsync<T>(T data, string topic) where T : class
+  {
+    ProducerConfig config = new()
+    {
+      BootstrapServers = _configuration["Kafka:BootstrapServers"],
+      ClientId = Dns.GetHostName()
+    };
+
+    try
+    {
+      using var producer = new ProducerBuilder<Null, string>(config).Build();
+      var message = JsonConvert.SerializeObject(data);
+      var result = await producer.ProduceAsync(topic,
+        new Message<Null, string>
+        {
+          Value = message
+        });
+
+      Debug.WriteLine($"Delivery Timestamp:{result.Timestamp.UtcDateTime}");
+      return await Task.FromResult(true);
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"Error occured: {ex.Message}");
+    }
+
+    return await Task.FromResult(false);
   }
 }
